2ea64243419d31204e42edbe7f41c352214f42e0
Naoya-Ikeda-jp <97081502+Naoya-Ikeda-jp@users.noreply.github.com>
2024-02-09 19:04:10 +0900

    Firefox
    プロキシオフ、自動更新オフなど

---
+++ Firefox_user_.js
@@ -0,0 +1,14 @@
+﻿# 追加
+user_pref("app.update.auto", false);
+user_pref("app.update.elevate.attempts", 0);
+user_pref("app.update.enabled", false);
+user_pref("app.update.lastUpdateTime.addon-background-update-timer", 0);
+user_pref("app.update.lastUpdateTime.background-update-timer", 0);
+user_pref("app.update.lastUpdateTime.blocklist-background-update-timer", 0);
+user_pref("app.update.service.enabled", false);
+user_pref("browser.search.update", false);
+user_pref("browser.shell.checkDefaultBrowser", false);
+user_pref("browser.startup.homepage", "http://10.1.**.**:8080/***/login_ja.html");
+user_pref("network.proxy.type", 0);
+# 
+user_pref("extensions.e10sBlockedByAddons", true);
